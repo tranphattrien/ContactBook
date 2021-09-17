ac02d9e3f67fca3d0249b86ae207f6701801ca24
trien <trientranphat@gmail.com>
2021-09-17 10:55:26 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const {BadRequestError} = require("./app/helpers/errors");
 
 const app = express();
+
 
 var corsOptions = {
     origin: "http://localhost:8081",
@@ -25,10 +27,15 @@ const PORT = config.app.port;
 app.listen(PORT, () => {
     console.log(`Server is running on port $(PORT).`);
 })
-
 setupContactRoutes(app);
-// set port, listen for requests
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+// Handle 4040 response
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and rotues calls
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
